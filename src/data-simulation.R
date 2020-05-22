5446ca396350fd775ad8e2c6c1c338fcc7014ff7
Mathieu Marauri <mathieu.marauri@mfglabs.com>
2020-05-22 15:02:09 +0200

    Generate barriers data to illustrate constrained IDW

--- src/data-simulation.R
+++ src/data-simulation.R
@@ -13,18 +13,18 @@ source("src/helpers.R") # Load custom functions
 # Set default ggplot theme
 theme_set(
   theme_light(
-  base_size = 20
+    base_size = 20
   ) +
-  theme(
-    text = element_text(family = "Gibson", colour = "gray10"),
-    panel.border = element_blank(),
-    axis.line = element_line(colour = "gray50", size = .5),
-    # axis.ticks = element_blank(),
-    strip.background = element_rect(colour = "gray50", fill = "transparent", size = .7),
-    strip.text.x = element_text(colour = "gray10"),
-    strip.text.y = element_text(colour = "gray10"),
-    legend.key.size = unit(1.5, "cm")
-  )
+    theme(
+      text = element_text(family = "Gibson", colour = "gray10"),
+      panel.border = element_blank(),
+      axis.line = element_line(colour = "gray50", size = .5),
+      # axis.ticks = element_blank(),
+      strip.background = element_rect(colour = "gray50", fill = "transparent", size = .7),
+      strip.text.x = element_text(colour = "gray10"),
+      strip.text.y = element_text(colour = "gray10"),
+      legend.key.size = unit(1.5, "cm")
+    )
 )
 
 # Set default scales
@@ -34,7 +34,7 @@ scale_fill_continuous <- function(...) ggplot2::scale_fill_viridis_c(..., option
 scale_fill_discrete <- function(...) ggplot2::scale_fill_viridis_d(..., option = "viridis")
 
 
-# Generation ----------------------------------------------------------------------------------
+# Simple data ---------------------------------------------------------------------------------
 
 # Data is generated using gaussian random fileds
 
@@ -68,3 +68,31 @@ plot_map(base_map, "Simulated data")
 
 # Export simulated data
 saveRDS(base_map, "data/grf_gauss_100x100.rds")
+
+
+# Barrier data --------------------------------------------------------------------------------
+
+# Generate sample data points on a region with barriers.
+
+# Generate some barriers
+barrier_1 <- rbind(c(0, .3), c(.3, .3), c(.3, .4), c(0, .4), c(0, .3))
+barrier_2 <- rbind(c(.5, .4), c(.55, .4), c(.75, .75), c(.7, .75), c(.5, .4))
+barrier_3 <- rbind(c(.7, .8), c(.7, 1), c(.3, 1), c(.3, .8), c(.7, .8))
+barriers <- st_multipolygon(list(list(barrier_1), list(barrier_2), list(barrier_3))) %>%
+  st_sfc() %>%
+  st_as_sf()
+
+# Generate some points
+points <- data.frame(
+  x = c(.1, .3, .5, .8, .55, .2, .8, .9, .05, .9, .1, .2, .05, .62),
+  y = c(0, .1, .2, .1, .6, .6, .3, .7, .65, .9, .9, .42, .43, .7),
+  z = c(-2, -1, 0, -1, 2, 2, 0, -1, 2, -2, 1, 2, 2, 1)
+)
+
+# Export the 2 objects
+saveRDS(barriers, "data/barriers.rds")
+saveRDS(points, "data/points.rds")
+
+
+
+
